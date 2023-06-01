3a3e0f27f07241453cdc232240858a97bf37bd3c
Rob Anderson <robanderson@hey.com>
2023-06-01 11:08:30 +0100

    fix yaml-notification

--- src/Scripts/notifications/invalid-yaml.ts
+++ src/Scripts/notifications/invalid-yaml.ts
@@ -1,6 +1,6 @@
 export class InvalidYamlNotification extends NotificationRequest {
   constructor() {
-    super("invalid-json");
+    super("invalid-yaml");
     this.title = nova.localize("Invalid YAML string");
     this.body = nova.localize("The selected string is not valid YAML");
   }
